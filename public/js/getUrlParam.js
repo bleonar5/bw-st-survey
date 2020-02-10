bebc835d627ae1d78920c92abefad8557b6fcb9b
ralph-payne <ralphpayne@gmail.com>
2020-02-10 12:38:58 +0000

    Troubleshoot redirect error with console logs

--- public/js/getUrlParam.js
+++ public/js/getUrlParam.js
@@ -22,10 +22,16 @@ window.addEventListener("load", function() {
     // On window load, take surveycode from url
     var surveyCodeFromUrl = getUrlParam('code','');
 
+    console.log(window.location.href);
+
     // If code exists, save in localStorage & redirect user to "/"
     if (surveyCodeFromUrl.length !== 0) {
         window.localStorage.setItem('code', surveyCodeFromUrl);
-        window.location.href = "/";
+        console.log('before redirect');
+        console.log(window.location.href);
+        window.location.href = "/", true;
+        console.log('after redirect');
+        console.log(window.location.href);
     // If code does not appear in url, then check that user already has code saved in localStorage. If user does not have a code saved in localStorage, set code as N/A. This can then be used to disable Amazon button when user reaches study-conclusion page.
     } else {
         let surveyCodeInLocalStorage = window.localStorage.getItem('code')
